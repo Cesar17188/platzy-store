05c193e0e9da79f98979321bc6494a53b715b3bd
Cesar Armendariz <cesareli17188@gmail.com>
2020-02-19 17:58:39 -0500

    contact finamlmente arreglado

--- src/app/contact/contact-routing.module.ts
+++ src/app/contact/contact-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ContactComponent } from './components/contact.component';
+import { ContactComponent } from '../contact/components/contact/contact.component';
 
 // tslint:disable-next-line: one-variable-per-declaration
 const routes: Routes = [
